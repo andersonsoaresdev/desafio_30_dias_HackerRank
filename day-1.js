32494681a7909231572caaaa1d52a386cd94a378
Anderson Soares <anderson@grupomundi.com.br>
2020-10-19 21:45:47 -0300

    Day 1: Data Types
    Utilizando tipos de dados, variáveis e as funções parseFloat, parseInt e toFixed.

---
+++ day-1.js
@@ -0,0 +1,32 @@
+
+/*
+Foi bem difícil completar esse teste pois o desafio é feito com o Java como linguagem principal,
+e o Java, ao contrário do JavaScript, possui o tipo de dados 'double'. Toda vez que submetia a
+resposta ele até ficava como correto no teste geral, mas falhava no teste oculto,
+porque a variável 'double' seria impressa no console como um número inteiro e não decimal. Da
+forma que fizemos deu certo, utilizando o parseFloat() e o toFixed() para utilizar corretamente
+as casas decimais.
+*/
+
+
+// Declare second integer, double, and String variables.
+var a, b, c
+
+// Read and save an integer, double, and String to your variables.
+a = parseInt(readLine())
+b = parseFloat(readLine())
+c = readLine()
+
+
+// Print the sum of both integer variables on a new line.
+console.log(i + a)
+
+// Print the sum of the double variables on a new line.
+console.log((d + b).toFixed(1))
+
+// Concatenate and print the String variables on a new line
+console.log(s + c)
+
+ // The 's' variable above should be printed first.
+
+}
