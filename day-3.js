e454193b233006751e517758bc8f988a96b45619
Anderson Soares <anderson@grupomundi.com.br>
2020-10-22 07:16:52 -0300

    Day 3: Intro to Conditional Statements
    In this challenge, we're getting started with conditional statements.

---
+++ day-3.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+
+}
+
+// Resposta do desafio a partir daqui : 
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    if (n % 2 == 1 && n >= 3) {
+        console.log("Weird")
+    } else if (n % 2 == 0 && n >= 2 && n <= 5) {
+        console.log("Not Weird")
+    } else if (n % 2 == 0 && n >= 6 && n <= 20) {
+        console.log("Weird")
+    } else {
+        console.log("Not Weird")
+    }
+
+}
