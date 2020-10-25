b1101f86cd75b9fb47da39fff7f769144d4243a7
Anderson Soares <anderson@grupomundi.com.br>
2020-10-25 19:10:21 -0300

    Day 5: Loops
    In this challenge, we're going to use loops to help us do some simple math.

---
+++ day-5.js
@@ -0,0 +1,10 @@
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    var i = 0;
+    for (i = 1; i <= 10; i++) {
+        var resultado = n * i;
+        console.log(n + " x " + i + " = " + resultado);
+    }
+
+}
